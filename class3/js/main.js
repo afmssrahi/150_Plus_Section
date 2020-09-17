d0c456e3589ab60df23b1a196d5a97cae3718d88
MD Shahriar Rahi <afmssrahi@gmail.com>
2020-09-17 07:04:06 +0600

    class3 done

---
+++ class3/js/main.js
@@ -0,0 +1,12 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function($) {
+        $(".video-play-btn").magnificPopup({
+            type: 'video',
+        });
+    });
+
+
+
+}  (jQuery));
